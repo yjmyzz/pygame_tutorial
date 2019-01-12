dbfd9b04f8e0463f95801e8ec690af0106d29614
yjmyzz@126.com <2city@4tory>
2019-01-12 22:29:04 +0800

    test

--- study_01.py
+++ study_01.py
@@ -1,19 +1,68 @@
 import pygame
 import sys
+import random
+import math
 
 pygame.init()
 
 SIZE = WIDTH, HEIGHT = 200, 400
-BLACK = 0, 0, 0
-RED = 255, 0, 0
-SPEED = [1, 1]
-angle = 1
+BACKGROUND_COLOR = (0, 0, 0)
 
 screen = pygame.display.set_mode(SIZE)
-originLeaf = pygame.image.load("leaf.png")
-originRect = originLeaf.get_rect()
+leaves = []
+
+
+class Leaf(object):
+    def __init__(self, pos=[10, 10], speed=[1, 1]):
+        self.imageSrc = pygame.image.load("leaf.png")
+        self.rect = self.imageSrc.get_rect()
+        self.image = self.imageSrc
+        self.speed = speed
+        self.angle = 0
+        self.pos = pos
+        self.rect = self.rect.move(pos[0], pos[1])
+
+    def move(self):
+        self.rect = self.rect.move(self.speed[0], self.speed[1])
+        new_rect = self.image.get_rect()
+        new_rect.left, new_rect.top = self.rect.left, self.rect.top
+        if new_rect.left < 0 or new_rect.right > WIDTH:
+            self.speed[0] = -self.speed[0]
+            if new_rect.right > WIDTH:
+                self.rect.left = WIDTH - new_rect.width
+            if new_rect.left < 0:
+                self.rect.left = 0
+        if new_rect.top < 0 or new_rect.bottom > HEIGHT:
+            self.speed[1] = -self.speed[1]
+            if new_rect.bottom > HEIGHT:
+                self.rect.top = HEIGHT - new_rect.height
+
+    def draw(self):
+        screen.blit(self.image, self.rect)
+
+    def rotate(self):
+        self.image = pygame.transform.rotate(self.imageSrc, self.angle)
+        self.angle += random.randint(1, 5)
+        if math.fabs(self.angle) == 360:
+            self.angle = 0
+
+
+def init():
+    for i in range(0, 3):
+        leaf = Leaf([random.randint(50, WIDTH - 50), random.randint(30, HEIGHT - 200)],
+                    [random.randint(1, 2), random.randint(1, 2)])
+        leaf.move()
+        leaves.append(leaf)
+
+
+def to255(x):
+    if x > 1:
+        x = 1
+    return int(255 * math.fabs(x))
+
 
 clock = pygame.time.Clock()
+init()
 
 while True:
 
@@ -21,38 +70,21 @@ while True:
         if event.type == pygame.QUIT:
             sys.exit()
 
-    # 利用矩形对象的move方法，让rect移动
-    originRect = originRect.move(SPEED[0], SPEED[1])
-    # 注意：这里一定要用一个新变量newLeaf，保存旋转后的image对象
-    newLeaf = pygame.transform.rotate(originLeaf, angle)
-    angle += 1
+    first_rect = leaves[0].rect
 
-    # 注意：这里要定义一个新rect对象，因为图象旋转后，其外切的矩形尺寸会变化
-    newRect = newLeaf.get_rect()
-    # 默认的newRect位置在(0,0)，要实现矩形外框跟随，必须赋值到新位置
-    newRect.left, newRect.top = originRect.left, originRect.top
+    # 根据第1片叶子的运动情况，随机切换背景色
+    color_r = to255(first_rect.top / HEIGHT)
+    color_g = to255(first_rect.left / WIDTH)
+    color_b = to255(math.fabs(first_rect.left) / (math.fabs(first_rect.top) + math.fabs(first_rect.left) + 1))
+    # print(color_r, color_g, color_b)
+    screen.fill((color_r, color_g, color_b))
 
-    # 左右边界反弹的处理
-    if newRect.left <= 0 or newRect.right >= WIDTH:
-        SPEED[0] = -SPEED[0]
-        # 图片移动到接近右边界时(比如:right=198)，由于旋转的作用，可能导致叶子一下横过来了，
-        # right突然会变成210，这样就算速度取反了，由于SPEED[0]=-1,需要10帧后，才能从视觉上真正看到反弹成功(即：210减到200，需要10次)
-        if newRect.right > WIDTH:
-            originRect.left = WIDTH - newRect.width
-
-    # 上下边界反弹的处理
-    if newRect.top <= 0 or newRect.bottom >= HEIGHT:
-        SPEED[1] = -SPEED[1]
-        # 类似右边界的校正处理，防止叶子接近下边界时，由于旋转，一下从横到竖，高度突然加大，导致越界
-        if newRect.bottom > HEIGHT:
-            originRect.top = HEIGHT - newRect.height
-
-    # 默认背景为白色，所以每渲染一帧，要对背景重新填充，否则会有上一帧的残影
-    screen.fill(BLACK)
-    # 画新矩形
-    pygame.draw.rect(screen, RED, newRect, 1)
     # 将旋转后的图象，渲染到新矩形里
-    screen.blit(newLeaf, originRect)
+    for item in leaves:
+        item.rotate()
+        item.move()
+        item.draw()
+
     # 正式渲染
     pygame.display.update()
     # 控制帧数<=100
